9fcb1c1bc815d40e676a60ade80b91f04f994b66
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-01 18:36:09 +0100

    Setup router and import route pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import Home from './pages/Home';
 import Quote from './pages/Quotes';
@@ -6,32 +7,49 @@ import NotMatch from './pages/NotMatch';
 import Navbar from './components/Navbar';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      digitObj: {
-        total: 0,
-        next: 0,
-        operation: null,
-      },
-    };
-    this.updateState = this.updateState.bind(this);
-  }
 
-  updateState(obj) {
-    this.setState({
-      digitObj: obj,
-    });
-  }
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+      <Route path="*" element={<NotMatch />} />
+    </Routes>
+  </Router>
+);
 
-  render() {
-    const { digitObj } = this.state;
-    return (
-      <div className="App">
-        <Calculator updateState={this.updateState} digitObj={digitObj} />
-      </div>
-    );
-  }
-}
 export default App;
+
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       digitObj: {
+//         total: 0,
+//         next: 0,
+//         operation: null,
+//       },
+//     };
+//     this.updateState = this.updateState.bind(this);
+//   }
+
+//   updateState(obj) {
+//     this.setState({
+//       digitObj: obj,
+//     });
+//   }
+
+//   render() {
+//     const { digitObj } = this.state;
+//     return (
+//       <div className="App">
+//         <Calculator updateState={this.updateState} digitObj={digitObj} />
+//       </div>
+//     );
+//   }
+// }
+// export default App;
