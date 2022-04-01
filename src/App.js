d1e64021c8c750156af446d9751c2b8980d99cf6
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-01 21:11:17 +0100

    Implement reviewer's corrections

--- src/App.js
+++ src/App.js
@@ -19,33 +19,3 @@ const App = () => (
 );
 
 export default App;
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       digitObj: {
-//         total: 0,
-//         next: 0,
-//         operation: null,
-//       },
-//     };
-//     this.updateState = this.updateState.bind(this);
-//   }
-
-//   updateState(obj) {
-//     this.setState({
-//       digitObj: obj,
-//     });
-//   }
-
-//   render() {
-//     const { digitObj } = this.state;
-//     return (
-//       <div className="App">
-//         <Calculator updateState={this.updateState} digitObj={digitObj} />
-//       </div>
-//     );
-//   }
-// }
-// export default App;
