f9fac22279f15af89aa15132d2fa1e50af572c77
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-02 12:07:19 +0100

    Implement unit test

--- src/__tests__/navBar.test.js
+++ src/__tests__/navBar.test.js
@@ -1,12 +1,6 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
-// import renderer from 'react-test-renderer';
 import NavBar from '../components/Navbar';
-
-// it ('Checking if Navigation Bar renders correctly', () => {
-//   const tree = renderer.create(<Router> <NavBar /></Router>).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
 
 describe('Test for Navbar component', () => {
   test('The component should render', () => {
